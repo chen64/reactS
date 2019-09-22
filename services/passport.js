bc630f68ade1de10f6b158fb5db3df5e285c9a6d
Jayson <jaysonchen64@gmail.com>
2019-09-21 22:35:59 -0500

    oath

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,15 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile token", profile);
+      User.findOne({ googleId: profile.id }).then(exisitingUser => {
+        if (exisitingUser) {
+          done(null, exisitingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => doNotTrack(null, user));
+        }
+      });
     }
   )
 );
